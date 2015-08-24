7631a7a70685c363d836a03bd68d8e68d7a0c0a6
rohan1592 <rohan1592@gmail.com>
2015-08-23 21:43:23 -0700

    Guessing game

--- The Guessing Game/ViewController.swift
+++ The Guessing Game/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var InputTextField: UITextField!
+    
+    @IBAction func SubmitButton(sender: AnyObject) {
+        
+        let random = String(arc4random_uniform(7));
+        print(random)
+        if(InputTextField.text == random)
+        {
+            OutputLabel.text = "You are correct!!"
+        }
+        else
+        {
+            OutputLabel.text = "You are wrong!!"
+        }
+    }
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
